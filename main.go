9462b1886adb27edfb404be45244fb0dcd1f10f0
Tahseen Jamal <tahseen.jamal@gmail.com>
2023-12-26 11:55:22 +0530

    Update main.go to use new message broker

--- main.go
+++ main.go
@@ -1,14 +1,15 @@
 package main
 
 import (
-	"sms_gateway/logger"
+	"fmt"
+	"sms_gateway/broker"
 )
 
 func main() {
 	// Initialize and start the consumer application
 
-	logging := logger.GetLumberJack()
-
-	logging.WriteLog("Hello World!")
+	activemq := broker.NewMessageBroker()
+	fmt.Println(activemq.Connect())
+	// fmt.Println(activemq.Subscribe("test_queue"))
 
 }
