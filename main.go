005758250a2f29f5c489d347db97dc49b828b2c0
Tahseen Jamal <tahseen.jamal@gmail.com>
2024-01-02 17:59:10 +0530

    Refactor logging message in main.go

--- main.go
+++ main.go
@@ -18,9 +18,7 @@ func main() {
 	// 	activemq.Send("test_queue", "test message")
 	// 	msg, err = activemq.Read("test_queue")
 	// 	if err == nil {
-	// 		activemq.FileLogger.WriteLog("Received message: " + msg)
-	// 	} else {
-	// 		activemq.FileLogger.WriteLog("Error reading message: " + err.Error())
+	// 		activemq.FileLogger.WriteLog("Received message: %s ", msg)
 	// 	}
 
 	// 	time.Sleep(1 * time.Second)
