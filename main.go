e32fd64b636280546cf5e153743f4c4f665b9b82
Tahseen Jamal <tahseen.jamal@gmail.com>
2024-01-03 15:40:20 +0530

    Add message broker functionality and update main function

--- main.go
+++ main.go
@@ -1,36 +1,47 @@
 package main
 
 import (
+	"fmt"
+	"sms_gateway/broker"
 	"sms_gateway/smppconnector"
 	"time"
 )
 
 func main() {
-	// Initialize and start the consumer application
-
-	// activemq := broker.NewMessageBroker()
-	// activemq.Subscribe("test_queue")
-	// for {
-
-	// 	var msg string
-	// 	var err error
-
-	// 	activemq.Send("test_queue", "test message")
-	// 	msg, err = activemq.Read("test_queue")
-	// 	if err == nil {
-	// 		activemq.FileLogger.WriteLog("Received message: %s ", msg)
-	// 	}
-
-	// 	time.Sleep(1 * time.Second)
-	// }
-
 	// Initialize smpp
 	smpp := smppconnector.NewSmpp()
 	smpp.Connect()
 
 	time.Sleep(1 * time.Second)
 
+	receiver := broker.NewMessageBroker()
+	receiver.Subscribe("http_calls")
+	sender := broker.NewMessageBroker()
+
+	var msg string
+	var err error
+
 	for {
-		_ = smpp.Send("test", "919899334417", "Hello", "")
+
+		err = sender.Send("http_calls", "test message")
+
+		if err == nil {
+			msg, err = receiver.Read("http_calls")
+			receiver.FileLogger.WriteLog("|HTTP|%s", msg)
+			if err == nil {
+				err = smpp.Send("test", "919899334417", "Hello", "")
+				if err != nil {
+					fmt.Println("SMPP Error sending message: ", err.Error())
+					time.Sleep(1 * time.Second)
+				}
+			}
+
+			time.Sleep(1 * time.Millisecond)
+		} else {
+			fmt.Println("Main: Error sending message: ", err.Error())
+			time.Sleep(1 * time.Second)
+		}
+
 	}
+
 }
