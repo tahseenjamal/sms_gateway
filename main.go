5509b3747d2e5182a3bcee4a328e624bde86b3d7
Tahseen Jamal <tahseen.jamal@gmail.com>
2024-01-05 17:46:07 +0530

    Add support for sending HTTP calls to SMPP gateway

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"net/url"
 	"sms_gateway/broker"
 	"sms_gateway/smppconnector"
 	"time"
@@ -17,18 +18,30 @@ func main() {
 	receiver.Subscribe("http_calls")
 	sender := broker.NewMessageBroker()
 
-	var msg string
 	var err error
+	var msg string
 
 	for {
 
-		err = sender.Send("http_calls", "test message")
+		data := "from=XtraCash&to=%2B254722000000&message=Hello%20World&test=false"
+
+		err = sender.Send("http_calls", data)
 
 		if err == nil {
 			msg, err = receiver.Read("http_calls")
 			receiver.FileLogger.WriteLog("|HTTP|%s", msg)
 			if err == nil {
-				err = smpp.Send("test", "919899334417", "Hello", "")
+
+				// Parse query string using net/url package
+				// and convert to map[string]string
+
+				q, err := url.ParseQuery(msg)
+				from := q.Get("from")
+				to := q.Get("to")
+				message, _ := url.QueryUnescape(q.Get("message"))
+				test := q.Get("test")
+
+				err = smpp.Send(from, to, message, test)
 				if err != nil {
 					fmt.Println("SMPP Error sending message: ", err.Error())
 					time.Sleep(1 * time.Second)
