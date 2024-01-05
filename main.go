1664bdb62d23905b5ada7b6e474ba496f8cec70a
Tahseen Jamal <tahseen.jamal@gmail.com>
2024-01-05 23:47:57 +0530

    Add request handler and refactor message sending logic

--- main.go
+++ main.go
@@ -4,54 +4,49 @@ import (
 	"fmt"
 	"net/url"
 	"sms_gateway/broker"
+	"sms_gateway/handler"
 	"sms_gateway/smppconnector"
+	"strings"
 	"time"
 )
 
 func main() {
 	// Initialize smpp
 	smpp := smppconnector.NewSmpp().WithRateLimit(2000).Connect()
-
 	time.Sleep(1 * time.Second)
 
 	receiver := broker.NewMessageBroker()
 	receiver.Subscribe("http_calls")
-	sender := broker.NewMessageBroker()
 
 	var err error
 	var msg string
 
-	for {
+	go func() {
 
-		data := "from=XtraCash&to=%2B254722000000&message=Hello%20World&test=false"
+		for {
 
-		err = sender.Send("http_calls", data)
-
-		if err == nil {
 			msg, err = receiver.Read("http_calls")
-			receiver.FileLogger.WriteLog("|HTTP|%s", msg)
 			if err == nil {
 
-				// Parse query string using net/url package
-				// and convert to map[string]string
-
 				q, err := url.ParseQuery(msg)
-				from := q.Get("from")
-				to := q.Get("to")
-				message, _ := url.QueryUnescape(q.Get("message"))
-				test := q.Get("test")
+				if err == nil {
 
-				err = smpp.Send(from, to, message, test)
-				if err != nil {
-					fmt.Println("SMPP Error sending message: ", err.Error())
-					time.Sleep(1 * time.Second)
+					from := q.Get("from")
+					to := strings.Trim(q.Get("to"), " ")
+					message := q.Get("message")
+					test := q.Get("test")
+
+					err = smpp.Send(from, to, message, test)
+					if err != nil {
+						fmt.Println("SMPP Error sending message: ", err.Error())
+						time.Sleep(1 * time.Second)
+					}
 				}
 			}
-		} else {
-			fmt.Println("Main: Error sending message: ", err.Error())
-			time.Sleep(1 * time.Second)
 		}
+	}()
 
-	}
+	requestHandler := handler.NewRequestHandler()
+	requestHandler.Listen()
 
 }
