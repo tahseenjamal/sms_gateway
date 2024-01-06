9b38cab05610e267bd2e6b317db6545644073577
Tahseen Jamal <tahseen.jamal@gmail.com>
2024-01-06 13:35:45 +0530

    Add error handling and reconnect functionality
    This commit adds error handling and reconnect functionality to the ActiveMQ broker implementation. It also includes a fix for a potential issue with reading messages.

--- broker/activemq.go
+++ broker/activemq.go
@@ -1,6 +1,7 @@
 package broker
 
 import (
+	"errors"
 	"sync"
 	"time"
 
@@ -64,6 +65,15 @@ func NewMessageBroker() *Activemq {
 	}
 
 	connection.Connect()
+
+	go func() {
+
+		for {
+			connection.Reconnect()
+		}
+
+	}()
+
 	return connection
 
 }
@@ -92,36 +102,32 @@ func (mb *Activemq) Connect() {
 	}
 }
 
-// Can also launch this separately as a goroutine to check if the connection is alive.
-// If not, it will reconnect. But if for any reasons the goroutine is killed, the connection status will not be checked.
-func (mb *Activemq) Reconnect(destination string) {
+func (mb *Activemq) ConnPointer() *stomp.Conn {
+	return mb.conn
+}
 
-	mutex.Lock()
-	defer mutex.Unlock()
+func (mb *Activemq) Reconnect() {
 
 	if mb.conn != nil {
 		t, e := mb.conn.Subscribe("/queue/heartbeat", stomp.AckAuto)
 		if e != nil {
-
 			mb.conn.Disconnect()
+			mb.conn = nil
 			mb.Connect()
-			if destination != "" {
-				mb.Subscribe(destination)
-			}
 		} else {
 			t.Unsubscribe()
 		}
 	}
+	time.Sleep(5 * time.Second)
 }
 
-// Sends a message to a specified destination.
+// Sends a message to a specified destination
+// TODO - Message persist or not persist
 func (mb *Activemq) Send(destination, body string) error {
 
 	err := mb.conn.Send(destination, "text/plain", []byte(body))
 	if err != nil {
 		mb.FileLogger.WriteLog("|BROKER_ERROR|Error sending to: %s", destination)
-		time.Sleep(1 * time.Second)
-		mb.Reconnect("")
 	}
 
 	return err
@@ -131,11 +137,14 @@ func (mb *Activemq) Send(destination, body string) error {
 func (mb *Activemq) Subscribe(destination string) {
 
 	var err error
-	mb.subs, err = mb.conn.Subscribe(destination, stomp.AckAuto)
+	if mb.ConnPointer() != nil {
+		mb.subs, err = mb.conn.Subscribe(destination, stomp.AckAuto)
+	} else {
+		err = errors.New("broker not connected")
+	}
 	if err != nil {
 		mb.FileLogger.WriteLog("|BROKER_ERROR|Error subscribing to: %s", destination)
 		time.Sleep(1 * time.Second)
-		mb.Reconnect(destination)
 	}
 }
 
@@ -143,13 +152,19 @@ func (mb *Activemq) Read(destination string) (string, error) {
 
 	var message *stomp.Message
 	var err error
-	message, err = mb.subs.Read()
-	if err != nil {
-		mb.FileLogger.WriteLog("Error reading from: %s", err.Error())
-		time.Sleep(1 * time.Second)
-		mb.Reconnect(destination)
-		return NULLSTRING, err
-	}
-	return string(message.Body), err
+	if mb.subs != nil {
+		message, err = mb.subs.Read()
+		if err != nil {
+			mb.FileLogger.WriteLog("Error reading from: %s", err.Error())
+			time.Sleep(1 * time.Second)
+			mb.Subscribe(destination)
+			return NULLSTRING, err
+		}
 
+		return string(message.Body), err
+	} else {
+		mb.Subscribe(destination)
+	}
+
+	return NULLSTRING, errors.New("broker not connected")
 }
